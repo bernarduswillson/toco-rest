6aa6276e57406ab1bf9808966c894fe1bedc81cf
ditramadia <ditrarizqaamadia@gmail.com>
2023-11-17 11:42:51 +0700

    feat: add api key

--- src/middleware/accessValidation.ts
+++ src/middleware/accessValidation.ts
@@ -1,36 +1,66 @@
 import { admin } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 interface ValidationRequest extends Request {
     adminData: admin;
 }
 
-const accessValidation = (req: Request, res: Response, next: NextFunction) => {
-    const validationReq = req as ValidationRequest
-    const {authorization} = validationReq.headers;
+const accessValidation = async (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.apiKey) {
+        let apiKey = req.query.apiKey;
 
-    // console.log('here: ', authorization)
+        try {
+            const response = await prisma.api_key.findUnique({
+                where: {
+                    key: String(apiKey),
+                },
+            });
 
-    if(!authorization){
-        return res.status(401).json({
-            message: 'Token not found'
-        })
-    }
-
-    const token = authorization.split(' ')[1];
-    const secret = process.env.JWT_SECRET!;
-
-    try {
-        const jwtDecode = jwt.verify(token, secret);
-
-        if(typeof jwtDecode !== 'string'){
-            validationReq.adminData = jwtDecode as admin;
+            console.log(response);
+            
+            if (response) {
+                console.log(apiKey);
+            } else {
+                return res.status(401).json({
+                    message: 'Unauthorized'
+                });
+            }
+        } catch (error) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
         }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Unauthorized'
-        })
+        
+    } else {
+        const validationReq = req as ValidationRequest
+        const {authorization} = validationReq.headers;
+
+        // console.log('here: ', authorization)
+
+        if(!authorization){
+            return res.status(401).json({
+                message: 'Token not found'
+            })
+        }
+
+        const token = authorization.split(' ')[1];
+        const secret = process.env.JWT_SECRET!;
+
+        try {
+            const jwtDecode = jwt.verify(token, secret);
+
+            if(typeof jwtDecode !== 'string'){
+                validationReq.adminData = jwtDecode as admin;
+            }
+        } catch (error) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            })
+        }
     }
     next()
 }
